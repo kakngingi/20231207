55fe06fd39e7ffe3142c09c9b3baa0b9c326791e
Woody <jsungj3@gmail.com>
2023-12-07 21:41:16 +0900

    Fix description by Woody

--- Project/src/com/javalec/function/AddSubMulDiv.java
+++ Project/src/com/javalec/function/AddSubMulDiv.java
@@ -3,7 +3,7 @@ package com.javalec.function;
 public class AddSubMulDiv extends AddSubMul {
 
 	/*
-	 * Description : AddSubMul로부터 상속받아 덧셈 + 뺼셈 + 곱셈 + 나눗셈을 출력하는 프로그램 
+	 * Description : AddSubMul로부터 상속받아 나눗셈을 출력하는 프로그램 
 	 * Date : 23.12.07.Thursday
 	 * Version : 1.0
 	 * Author : Woody Jo
