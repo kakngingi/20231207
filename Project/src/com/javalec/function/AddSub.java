763876e95566806a747555ddfeba43aefb8a5f56
Taewoo Ki <ilml2@daum.net>
2023-12-07 15:26:26 +0900

    update

--- Project/src/com/javalec/function/AddSub.java
+++ Project/src/com/javalec/function/AddSub.java
@@ -1,36 +1,41 @@
 package com.javalec.function;
 
+import com.javalec.function.Add;
+
 public class AddSub extends Add{
 	
   	// Field
 	
 		 
-			;
-	AddSub as = new AddSub(10, 20, 30);
+			
+
 	
 	 // Constructor
 	 
 	 public AddSub() {
 		 
 	 }
-	 public AbbSub(int a, int b, int c) {
-		 
-		 int sub = a - b - c;
-		 System.out.println(sub);
-		 
-		 
+	
+	 public AddSub(int num1, int num2) {
+		 int sub1 = num1-num2;
+		 System.out.println(sub1);
 	 }
 	 
-	public AddSub(int num1, int num2, int num3) {
-		this.num1 = num1;
-		this.num2 = num2;
-		this.num3 = num3;
-		this.num4 = num4;
-	}
-	
-	 public void sum( ) {
-		 System.out.println(num1 - num2 - num3 - num4);
+	 public AbbSub(int num1, int num2, int num3) {
 		 
+		 int sub2 = num1 - num2 - num3;
+		 System.out.println(sub2);
+	 
+	 }
+	 
+		
+	// Method
+	
+	 public void AddSubResult1( ) {
+		 return num1- num2 -num3;
+	 }
+	 public void AddSubResult2() {
+		 return num1 -num2;
 	 }
 	 
 
