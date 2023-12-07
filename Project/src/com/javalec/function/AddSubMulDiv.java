680faf816e7c62c1a7851567a481606fb8a497d3
Woody <jsungj3@gmail.com>
2023-12-07 14:19:13 +0900

    Div version 1.0 by Woody

--- Project/src/com/javalec/function/AddSubMulDiv.java
+++ Project/src/com/javalec/function/AddSubMulDiv.java
@@ -1,36 +1,30 @@
 package com.javalec.function;
 
 public class AddSubMulDiv extends AddSubMul {
-	
-/*
-	 * Description : AddSubMul로부터 상속받아 덧셈 + 뺼셈 + 곱셈 + 나눗셈을 출력하는 프로그램
-	 * Date : 23.12.07.Thursday
-	 * Version : 1.0
-	 * Author : Woody Jo
- */
-	
-	
+
+	/*
+	 * Description : AddSubMul로부터 상속받아 덧셈 + 뺼셈 + 곱셈 + 나눗셈을 출력하는 프로그램 Date :
+	 * 23.12.07.Thursday Version : 1.0 Author : Woody Jo
+	 */
+
 	// Field
-	
+
 	int div;
-	int firstNum;
-	int secondNum;
-	
+
 	// Constructor
-	public AddSubMulDiv () {
-		
+	public AddSubMulDiv() {
+		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
+
 	// Method
-	public void addSubMulDiv (int firstNum, int secondNum) {
-		div = firstNum / secondNum;
-		
-		System.out.println(AddSubMul.add + AddSubMul.addSub + AddSubMul.addSubMul + div);
+	public void div(int n1, int n2, int n4) {
+//		super.mul();
+		if (n1 > Integer.MIN_VALUE)
+			div = n1 / n2 / n4;
+		else
+			div = n2 / n4;
+		System.out.println(div);
+
 	}
-	
-	
-	
 
 }
