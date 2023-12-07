3700fbe4fd6f9bf70907bee57c2a2632fa1b6419
kimsor1 <imoiksr@gmail.com>
2023-12-07 15:28:52 +0900

    addsub 수정

--- Project/src/com/javalec/function/AddSub.java
+++ Project/src/com/javalec/function/AddSub.java
@@ -1,42 +1,35 @@
 package com.javalec.function;
 
-import com.javalec.function.Add;
+public class AddSub extends Add {
 
-public class AddSub extends Add{
-	
-  	// Field
-	
-		 
-			
+	// Field
 
-	
-	 // Constructor
-	 
-	 public AddSub() {
-		 
-	 }
-	
-	 public AddSub(int num1, int num2) {
-		 int sub1 = num1-num2;
-		 System.out.println(sub1);
-	 }
-	 
-	 public AbbSub(int num1, int num2, int num3) {
-		 
-		 int sub2 = num1 - num2 - num3;
-		 System.out.println(sub2);
-	 
-	 }
-	 
-		
+	// Constructor
+
+	public AddSub() {
+
+	}
+
+	public AddSub(int num1, int num2) {
+		int sub1 = num1 - num2;
+		System.out.println(sub1);
+	}
+
+	public int AbbSub(int num1, int num2, int num3) {
+
+		int sub2 = num1 - num2 - num3;
+		return sub2;
+
+	}
+
 	// Method
-	
-	 public void AddSubResult1( ) {
-		 return num1- num2 -num3;
-	 }
-	 public void AddSubResult2() {
-		 return num1 -num2;
-	 }
-	 
+
+	public int AddSubResult1() {
+		return num1 - num2 - num3;
+	}
+
+	public int AddSubResult2() {
+		return num1 - num2;
+	}
 
 }
