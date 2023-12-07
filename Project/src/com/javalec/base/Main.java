5cdcc5bae461c9409476261c71c61a1301746b67
kimsor1 <imoiksr@gmail.com>
2023-12-07 13:55:11 +0900

    main이랑 add 작성

--- Project/src/com/javalec/base/Main.java
+++ Project/src/com/javalec/base/Main.java
@@ -1,9 +1,36 @@
 package com.javalec.base;
 
+import java.util.*;
+
+import com.javalec.function.Add;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("숫자 1을 입력하세요 : ");
+		int num1 = sc.nextInt();
+
+		System.out.print("숫자 2을 입력하세요 : ");
+		int num2 = sc.nextInt();
+
+		System.out.print("숫자 3을 입력하세요 : ");
+		int num3 = sc.nextInt();
+
+		System.out.print("숫자 4을 입력하세요 : ");
+		int num4 = sc.nextInt();
+
+		Add add = new Add();
+		add.num1 = num1;
+		add.num2 = num2;
+		add.num3 = num3;
+		add.num4 = num4;
+
+		System.out.println(">>>Feild를 사용");
+		System.out.println(num1 + "과 " + num2 + "의 숫자 합은 " + add.addResult() + "입니다.");
 
 	}
 
--- Project/src/com/javalec/function/Add.java
+++ Project/src/com/javalec/function/Add.java
@@ -2,25 +2,19 @@ package com.javalec.function;
 
 public class Add {
 	// Field
-	public int firstNum;
-	public int secondNum;
-	
-	
+	public int num1;
+	public int num2;
+	public int num3;
+	public int num4;
+
 	// Constructor
 	public Add() {
 		// TODO Auto-generated constructor stub
 	}
-	public Add(int firstNum, int secondNum) {
-		
-		this.firstNum = firstNum;
-		this.secondNum = secondNum;
-	}
-	public void sum() {
-		System.out.println(firstNum + secondNum);
-	}
-	
-	
+
 	// Method
-	
+	public int addResult() {
+		return num1 + num2;
+	}
 
 }
