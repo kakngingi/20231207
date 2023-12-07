cbb097ae0348f6a37eef9c3d77c0ef25e4052a6d
Taewoo Ki <ilml2@daum.net>
2023-12-07 14:49:52 +0900

    addsub 초안

--- Project/src/com/javalec/function/AddSub.java
+++ Project/src/com/javalec/function/AddSub.java
@@ -1,5 +1,39 @@
-package com.javalec.function;
+package com.javalec.funtion;
 
-public class AddSub {
+import com.javalec.funtion.Add;
+
+public class AddSub extends Add{
+	
+  	// Field
+	
+		 
+			;
+	AddSub as = new AddSub(10, 20, 30);
+	
+	 // Constructor
+	 
+	 public AddSub() {
+		 
+	 }
+	 public AbbSub(int a, int b, int c) {
+		 
+		 int sub = a - b - c;
+		 System.out.println(sub);
+		 
+		 
+	 }
+	 
+	public AddSub(int num1, int num2, int num3) {
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+		this.num4 = num4;
+	}
+	
+	 public void sum( ) {
+		 System.out.println(num1 - num2 - num3 - num4);
+		 
+	 }
+	 
 
 }
