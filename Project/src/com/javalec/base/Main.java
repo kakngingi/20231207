1f64833bf6359231b72d2a8c8405e189f811c1e2
kimsor1 <imoiksr@gmail.com>
2023-12-07 15:37:11 +0900

    main 최종, addsub 수정

--- Project/src/com/javalec/base/Main.java
+++ Project/src/com/javalec/base/Main.java
@@ -2,7 +2,7 @@ package com.javalec.base;
 
 import java.util.*;
 
-import com.javalec.function.Add;
+import com.javalec.function.AddSubMulDivRemainder;
 
 public class Main {
 
@@ -23,26 +23,47 @@ public class Main {
 		System.out.print("숫자 4을 입력하세요 : ");
 		int num4 = sc.nextInt();
 
-		Add add = new Add();
-		add.num1 = num1;
-		add.num2 = num2;
-		add.num3 = num3;
-		add.num4 = num4;
+		AddSubMulDivRemainder calc = new AddSubMulDivRemainder();
+		calc.num1 = num1;
+		calc.num2 = num2;
+		calc.num3 = num3;
+		calc.num4 = num4;
 
 		System.out.println(">>> Feild를 사용");
-		System.out.println("1. " + num1 + "과 " + num2 + "의 숫자 합은 " + (add.num1 + add.num2) + "입니다.");
-		System.out.println("2. " + num1 + "과 " + num3 + ", " + num4 + "의 *덧셈 뺄셈 곱셈 나눗셈* 의 결과는 " + "\n덧셈 : "
-				+ (add.num1 + add.num3 + add.num4) + "\n뺄셈 : " + (add.num1 - add.num3 - add.num4) + "\n곱셈 : "
-				+ (add.num1 * add.num3 * add.num4) + "\n나눗셈 : " + (double) (add.num1 / add.num3 / add.num4));
+		System.out.println("1. " + num1 + "과 " + num2 + "의 숫자 합은 " + (calc.num1 + calc.num2) + "입니다.");
+		System.out.println("2. " + num1 + "과 " + num3 + ", " + num4 + "의 *덧셈 뺄셈 곱셈 나눗셈* 의 결과는 ");
+		System.out.println("덧셈 : " + (calc.num1 + calc.num3 + calc.num4));
+		System.out.println("뺄셈 : " + (calc.num1 - calc.num3 - calc.num4));
+		System.out.println("곱셈 : " + (calc.num1 * calc.num3 * calc.num4));
+		System.out.println("나눗셈 : " + (calc.num1 / calc.num3 / calc.num4));
+		System.out.println("---------------------------------------------------------------------");
 
 		System.out.println(">>> Constuctor를 사용");
-		System.out.println("1. " + num3 + "과 " + num4 + "번의 *덧셈, 뺄셈, 곱셈* 의 결과는 덧셈 : " + add.Add(num3, num4));
-		System.out.println("2. " + num1 + ", " + num2 + ", " + num3 + "의 *뺄셈과 곱셈* 의 결과는 뺄셈 : ");
+		System.out.println("1. " + num3 + "과 " + num4 + "번의 *덧셈, 뺄셈, 곱셈* 의 결과는 ");
+		System.out.println("덧셈 : " + calc.Add(num3, num4));
+		System.out.println("뺄셈 : " + calc.AddSub(num3, num4));
+		System.out.println("곱셈 : " + calc.AddSubMul(num3, num4));
+
+		System.out.println("2. " + num1 + ", " + num2 + ", " + num3 + "의 *뺄셈과 곱셈* 의 결과는");
+		System.out.println("뺄셈 : " + calc.AddSub(num1, num2, num3));
+		System.out.println("곱셈 : " + calc.AddSubMul(num1, num2, num3));
+		System.out.println("---------------------------------------------------------------------");
 
 		System.out.println(">>> Method를 사용");
-		System.out.println(
-				"1. " + num1 + "," + num2 + "," + num4 + "의 *덧셈, 뺄셈, 곱셈, 나눗셈, 나머지*의 결과는 덧셈: " + add.addResult());
-		System.out.println("2. " + num2 + "번과 " + num4 + "번의 *뺄셈과 나눗셈* 의 결과는 뺄셈 : ");
+		System.out.println("1. " + num1 + "," + num2 + "," + num4 + "의 *덧셈, 뺄셈, 곱셈, 나눗셈, 나머지*의 결과는");
+		System.out.println("덧셈: " + calc.addResult());
+		System.out.println("뺄셈: " + calc.AddSubResult1());
+		System.out.println("곱셈: " + calc.AddSumMulresult());
+		System.out.print("나눗셈: ");
+		calc.div(num1, num2, num4);
+		System.out.print("나머지 : ");
+		calc.remainder(num1, num2, num4);
+
+		System.out.println("2. " + num2 + "번과 " + num4 + "번의 *뺄셈과 나눗셈* 의 결과는");
+		System.out.println("뺄셈 : " + calc.AddSubResult2());
+		System.out.print("나눗셈 : ");
+		calc.div(num2, num4);
+		System.out.println("---------------------------------------------------------------------");
 
 	}
 
--- Project/src/com/javalec/function/AddSub.java
+++ Project/src/com/javalec/function/AddSub.java
@@ -10,15 +10,12 @@ public class AddSub extends Add {
 
 	}
 
-	public AddSub(int num1, int num2) {
-		int sub1 = num1 - num2;
-		System.out.println(sub1);
+	public int AddSub(int num1, int num2) {
+		return num1 - num2;
 	}
 
-	public int AbbSub(int num1, int num2, int num3) {
-
-		int sub2 = num1 - num2 - num3;
-		return sub2;
+	public int AddSub(int num1, int num2, int num3) {
+		return num1 - num2 - num3;
 
 	}
 
