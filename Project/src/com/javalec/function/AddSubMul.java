90a659ddcb47d36f6ee43ceded9abd8ca5595a94
Kanbeomshiek <tvzz1105@naver.com>
2023-12-07 14:55:35 +0900

    2차 곱셈Class

--- Project/src/com/javalec/function/AddSubMul.java
+++ Project/src/com/javalec/function/AddSubMul.java
@@ -1,89 +1,43 @@
 package com.javalec.function;
 
-public class AddSubMul extends AddSub{
+public class AddSubMul extends AddSub {
 
 	
-	   //Field 
-	
-	
-//			int num1;
-//			int num2; 							
-//			int num3;
-//		 int Mul ;
-//	     int Mul2;
-//	     int Mul3;
-//		public int num1; 
-//		public int num2;
-			
-		public int num1;
-		public int num2;
-		public int num3;
-		public int num4;
+	/*
+	 *  메인에서 받은 숫자를 이용하여 곱셈을 하여 다시 메인으로 보내기
+	 *  Date : 2023.12.07.목요일
+	 *  Version : 1.0
+	 *  Author : 강범식
+	 */
 	
 	
 	
-	  //constructor
 	
-			   public AddSubMul () {
-			    	
-			   }
-				       
-			 
-			   public void AddSumMul(int num1 , int num2, int num3 ) {
-				   System.out.println( num1*num2*num3);
-				
-					   
-			   }
-			    	
-			   	
-//			   public AddSubMul(int mul , int mul2 , int mu3) {
-//				   super();
-//				  this.Mul = mul;
-//			     this.Mul2 = mul2;
-				
-					
+	
+	// Field
+
+	// constructor
+
+	public AddSubMul() {						
+
+	}
+
+
+	public int AddSubMul(int num1, int num2 ,int num3) {							//숫자 3개를 입력받고 constructor 에서 곱셈하여 다시 메인으로 보내기
+		 return super.num1*num2*num3;
+		 
+	public int AddSubMul(int num1 , int num2 ) {
+		return super.num3*num4;
+		
+	}
+			}
+
+	// Method
+
+	public int AddSumMulresult( ) {													//숫자 3개를 입력받고 Method 에서 곱셈하여 다시 메인으로 보내기
+			return  ( num1*num2*num4);
 			
 			
-	
-	
-	
-	
+			}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//Method
-	
-			    
-			    
-//	    public void Mul (int num1 , int num2 ,int num3 ,int num4 ) {
-//			    			mul1(num1,num2);
-//			    			mul2(num1,num2,num3);
-//			    			mul3(num1*num2*num3*num4);
-//			    			
-//			   private void mul () {
-//				   System.out.println(num1*num2);
-//				   
-				   
-//			   }
-	    	
-//	    }
-	
-	
-	
-	
-	
-	
-	
 }
